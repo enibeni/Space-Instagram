7a721ed2969cd63de3c901f468283d56c39aeb7d
Sergey Morozov <se.mrzv@gmail.com>
2019-04-25 08:15:45 +0300

    Fixing review comments

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -4,23 +4,33 @@ import os
 
 def download_image(image_url, path_to_image):
     directory = './images/'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    os.makedirs(directory, exist_ok=True)
 
     response = requests.get(image_url)
-    with open(path_to_image, 'wb') as file:
-        file.write(response.content)
+    if response.ok:
+        with open(path_to_image, 'wb') as file:
+            file.write(response.content)
 
 
 def fetch_hubble_image_by_id(id, path_to_image):
     hubble_image_urls = f'http://hubblesite.org/api/v3/image/{id}'
-    hubble_image_url = requests.get(hubble_image_urls).json()['image_files'][-1:][0]['file_url']
+    hubble_image_url = ""
+    response = requests.get(hubble_image_urls)
+    if response.ok:
+        hubble_image_url = response.json()['image_files'][-1:][0]['file_url']
     image_ext = get_image_ext(hubble_image_url)
     download_image(hubble_image_url, f'{path_to_image}/hubble{id}.{image_ext}')
 
 
 def get_hubble_images_id_by_collection(collection_name):
-    r = requests.get(f'http://hubblesite.org/api/v3/images?page=all&collection_name={collection_name}')
+    params = {
+        "collection_name": collection_name,
+        "page": "all"
+    }
+    r = requests.get(
+        url='http://hubblesite.org/api/v3/images',
+        params=params
+    )
     image_ids = [x['id'] for x in r.json()]
     return image_ids
 
@@ -33,5 +43,5 @@ if __name__ == '__main__':
     path_to_images = './images'
     collection_name = "spacecraft"
     image_ids = get_hubble_images_id_by_collection(collection_name=collection_name)
-    for id in image_ids:
-        fetch_hubble_image_by_id(id, path_to_images)
+    for image_id in image_ids:
+        fetch_hubble_image_by_id(image_id, path_to_images)
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -4,19 +4,22 @@ import os
 
 def fetch_spacex_last_launch(path_to_images):
     spacex_launch_url = 'https://api.spacexdata.com/v3/launches/latest'
-    spacex_images_urls = requests.get(spacex_launch_url).json()['links']['flickr_images']
+    spacex_images_urls = []
+    response = requests.get(spacex_launch_url)
+    if response.ok:
+        spacex_images_urls = response.json()['links']['flickr_images']
     for img_number, url in enumerate(spacex_images_urls):
         download_image(url, f'{path_to_images}/spacex{img_number}.jpg')
 
 
 def download_image(image_url, path_to_image):
     directory = './images/'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    os.makedirs(directory, exist_ok=True)
 
     response = requests.get(image_url)
-    with open(path_to_image, 'wb') as file:
-        file.write(response.content)
+    if response.ok:
+        with open(path_to_image, 'wb') as file:
+            file.write(response.content)
 
 
 if __name__ == '__main__':
